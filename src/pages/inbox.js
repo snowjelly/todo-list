bdc6eee6f14ed91c07cb9b8536e9baa67880aaf7
Connor Broadwell <broadwellconnor@gmail.com>
2022-11-01 09:10:22 -0500

    viewHeader IIFE for organization

--- src/pages/inbox.js
+++ src/pages/inbox.js
@@ -5,18 +5,20 @@ const inbox = () => {
   const viewContainer = document.createElement('div');
   viewContainer.id = 'view-container';
 
-  const viewHeaderContainer = document.createElement('div');
-  viewHeaderContainer.id = 'view-header-container';
-
-  viewContainer.appendChild(viewHeaderContainer);
-
-  const viewHeaderContent = document.createElement('div');
-  viewHeaderContent.id = 'view-header-content';
-  viewHeaderContent.innerHTML = `
-  <h1>viewHeader</h1>
-  `;
-
-  viewHeaderContainer.appendChild(viewHeaderContent);
+  const viewHeader = (() => {
+    const viewHeaderContainer = document.createElement('div');
+    viewHeaderContainer.id = 'view-header-container';
+  
+    viewContainer.appendChild(viewHeaderContainer);
+  
+    const viewHeaderContent = document.createElement('div');
+    viewHeaderContent.id = 'view-header-content';
+    viewHeaderContent.innerHTML = `
+    <h1>viewHeader</h1>
+    `;
+  
+    viewHeaderContainer.appendChild(viewHeaderContent);
+  })();
 
   content.appendChild(viewContainer);
 }
