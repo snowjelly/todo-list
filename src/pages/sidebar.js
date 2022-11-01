4b277e3751f34d4032784994d55ed4833dd88983
Connor Broadwell <broadwellconnor@gmail.com>
2022-11-01 09:43:33 -0500

    top-menu

--- src/pages/sidebar.js
+++ src/pages/sidebar.js
@@ -3,6 +3,23 @@ const sidebarDiv = () => {
   const sidebarContainer = document.createElement('div');
   sidebarContainer.id = 'sidebar-container';
 
+  const sidebarContent = document.createElement('div');
+  sidebarContent.id = 'sidebar-content';
+  
+  const topMenu = (() => {
+    const topMenu = document.createElement('ul');
+    topMenu.id = 'top-menu';
+
+    const inboxLi = document.createElement('li');
+    inboxLi.id = 'filter-inbox';
+
+    topMenu.appendChild(inboxLi);
+
+    sidebarContent.appendChild(topMenu);
+  })();
+
+  sidebarContainer.appendChild(sidebarContent);
+
   return sidebarContainer;
 }
 
