4f3c2a92616698b746e16e0f8e1c756fadbf916c
Connor Broadwell <broadwellconnor@gmail.com>
2022-11-02 15:05:02 -0500

    add task styling

--- src/pages/inbox.js
+++ src/pages/inbox.js
@@ -37,48 +37,49 @@ const inbox = () => {
     viewList.id = 'view-list';
 
     const todoListItems = (() => {
-      const todoListItem = document.createElement('li');
-      todoListItem.classList.value = 'todo-list-item';
+      for (let i=0;i<5;i++) {
+        const todoListItem = document.createElement('li');
+        todoListItem.classList.value = 'todo-list-item';
+    
+        const todoListItemCheckbox = document.createElement('div');
+        todoListItemCheckbox.classList.value = 'checkbox';
+    
+        todoListItem.appendChild(todoListItemCheckbox);
+    
+        const todoListItemText = document.createElement('p');
+        todoListItemText.textContent = `
+        Lorem ipsum dolor sit amet.`;
   
-      const todoListItemCheckbox = document.createElement('div');
-      todoListItemCheckbox.classList.value = 'checkbox';
-  
-      todoListItem.appendChild(todoListItemCheckbox);
-  
-      const todoListItemText = document.createElement('p');
-      todoListItemText.textContent = `
-      Lorem ipsum dolor sit amet.`;
-
-      todoListItem.appendChild(todoListItemText);
-  
-      viewList.appendChild(todoListItem);
+        todoListItem.appendChild(todoListItemText);
+    
+        viewList.appendChild(todoListItem);
+      }
     })();
     
     viewListContent.appendChild(viewList);
 
     viewListContainer.appendChild(viewListContent);
+
+    const addTask = (() => {
+      const addTaskContent = document.createElement('div');
+      addTaskContent.id = 'add-task-content';
+      
+      const addTaskImage = document.createElement('div');
+      addTaskImage.id = 'add-task-image';
+    
+      addTaskContent.appendChild(addTaskImage);
+    
+      const addTaskText = document.createElement('p');
+      addTaskText.id = 'add-task-text';
+      addTaskText.textContent = 'Add task';
+    
+      addTaskContent.appendChild(addTaskText);
+
+      viewListContainer.appendChild(addTaskContent);
+    })();
   })();
 
-  const addTaskContainer = document.createElement('div');
-  addTaskContainer.id = 'add-task-container';
-
-  const addTaskContent = document.createElement('div');
-  addTaskContent.id = 'add-task-content';
-
-  addTaskContainer.appendChild(addTaskContent);
   
-  const addTaskImage = document.createElement('div');
-  addTaskImage.id = 'add-task-image';
-
-  addTaskContent.appendChild(addTaskImage);
-
-  const addTaskText = document.createElement('p');
-  addTaskText.id = 'add-task-text';
-  addTaskText.textContent = 'Add task';
-
-  addTaskContent.appendChild(addTaskText);
-
-  viewContent.appendChild(addTaskContainer);
 
   content.appendChild(viewContainer);
 }
